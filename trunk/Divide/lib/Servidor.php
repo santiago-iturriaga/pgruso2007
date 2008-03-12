f426dae8ae9ab49425f2aff552dd6de225c3b6f5
damianpintos <damianpintos@0bfa85fe-dc32-0410-8b36-639fd1f935ce>
2008-03-12 22:18:47 +0000

    x

--- trunk/Divide/lib/Servidor.php
+++ trunk/Divide/lib/Servidor.php
@@ -4,6 +4,10 @@ include_once("Constantes.php");
 function ejecutar_servidor($comando,$usuario=USERNAME){
 	$command = SSH." -l ".$usuario." ".HOST." \"".$comando."; exit\" 2>&1";
 	$res = `$command`;
+if($res!=""){
+	error_log("COMANDO: ".$command);
+	error_log("SALIDA: ".$res);
+}
 	return $res;
 }
 ?>
